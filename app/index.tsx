bdf0631a7c39cad6705442efdb1f220c493a8e7d
Dat <dat.ta@gravityglobal.com>
2026-01-25 23:06:30 +0700

    feat(package): set up native wind

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,13 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to NativeWind!
+      </Text>
+      <Text className="mt-4 text-gray-600">
+        Edit app/index.tsx to edit this screen.
+      </Text>
     </View>
   );
 }
