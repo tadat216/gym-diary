e9989d5afd15fac15973f5b62b710a8b3647b411
Dat <dat.ta@gravityglobal.com>
2026-01-26 01:06:57 +0700

    feat(metro): add .sql to source extensions for Drizzle migrations

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,7 @@ const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
+// Add .sql to source extensions for Drizzle migrations
+config.resolver.sourceExts.push("sql");
+
 module.exports = withNativeWind(config, { input: "./global.css" });
