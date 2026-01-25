242386ada2b36b4a4b00e8752c62eef6f24c1867
Dat <dat.ta@gravityglobal.com>
2026-01-26 01:08:01 +0700

    feat(schema): add initial database schema for muscle groups, exercises, workouts, and details

---
+++ db/schema.ts
@@ -0,0 +1,42 @@
+import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
+
+export const muscle_groups = sqliteTable("muscle_groups", {
+  id: int("id").primaryKey({ autoIncrement: true }),
+  name: text("name").notNull().unique(),
+  color_hex: text("color_hex").notNull(),
+});
+
+export const exercises = sqliteTable("exercises", {
+  id: int("id").primaryKey({ autoIncrement: true }),
+  name: text("name").notNull(),
+  muscle_group_id: int("muscle_group_id")
+    .notNull()
+    .references(() => muscle_groups.id),
+});
+
+export const workouts = sqliteTable("workouts", {
+  id: int("id").primaryKey({ autoIncrement: true }),
+  date: text("date").notNull(),
+});
+
+export const workout_exercises = sqliteTable("workout_exercises", {
+  id: int("id").primaryKey({ autoIncrement: true }),
+  workout_id: int("workout_id")
+    .notNull()
+    .references(() => workouts.id),
+  exercise_id: int("exercise_id")
+    .notNull()
+    .references(() => exercises.id),
+});
+
+export const workout_exercises_details = sqliteTable(
+  "workout_exercises_details",
+  {
+    id: int("id").primaryKey({ autoIncrement: true }),
+    workout_exercise_id: int("workout_exercise_id")
+      .notNull()
+      .references(() => workout_exercises.id),
+    rep_count: int("rep_count").notNull(),
+    weight: int("weight").notNull(),
+  },
+);
